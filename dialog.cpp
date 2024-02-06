fa972dc99aaa4a33adc9bcfadfa0671971743a28
tsvigo <11_70@bk.ru>
2024-02-06 06:37:56 +0300

    2

--- dialog.cpp
+++ dialog.cpp
@@ -1,7 +1,14 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include "QStorageInfo"
+#include "QThread"
+#include <QProcess>
+
+//#include <QMediaPlayer>
 // QStorageInfo storage(qApp->applicationDirPath());
+int peremennaya_polnoe_prostranstvo;
+int peremennaya_dostupnoe_prostranstvo;
+int protsent_dostupnogo_prostranstva;
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Dialog)
@@ -14,18 +21,29 @@ Dialog::Dialog(QWidget *parent)
 //            if (!storage.isReadOnly()) {
                 // ...
   //  QStorageInfo::setPath(const QString &path)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// \brief path звук
+//    QMediaPlayer player; 
+
+//        player.setMedia(QUrl::fromLocalFile("/home/viktor/Загрузки/signal-elektronnogo-budilnika-33304.mp3")); //this seems to be default alert
+//    player.setVolume(50); 
+
+//        player.play();
+
+   
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const QString &path="/home";
   //  &path="/home";
-             //   QStorageInfo storage =  
+          // QStorageInfo storage =  
                 //    QStorageInfo storage(qApp->applicationDirPath());
                         //https://spec-zone.ru/qt~5.9/qstorageinfo
                       //  "/home";
-  // QStorageInfo::root(); // правильный https://stackoverflow.com/questions/1732717/how-to-determine-how-much-free-space-on-a-drive-in-qt
+ // QStorageInfo storage = QStorageInfo::root(); // правильный https://stackoverflow.com/questions/1732717/how-to-determine-how-much-free-space-on-a-drive-in-qt
               
  //  QStorageInfo::setPath(//"/home"
                        //  path
                         // );
-    QStorageInfo storage(qApp->applicationDirPath());
+    QStorageInfo storage(qApp->applicationDirPath()); // правильный 2-/home
                ui->textEdit->append (storage.rootPath());
              
                 if (storage.isReadOnly())
@@ -61,7 +79,10 @@ void Dialog::updateTime()
    //  ui->textEdit->append ( ".");
   //  ui->label->setText(QTime::currentTime().toString());
     // обновим текстовое поле
-    QStorageInfo storage(qApp->applicationDirPath());
+  
+   QStorageInfo storage(qApp->applicationDirPath()); // /home
+ // QStorageInfo storage = QStorageInfo::root(); // корень - /
+    // правильный https://stackoverflow.com/questions/1732717/how-to-determine-how-much-free-space-on-a-drive-in-qt
     ui->textEdit->append (storage.rootPath());
     
     if (storage.isReadOnly())
@@ -73,7 +94,7 @@ void Dialog::updateTime()
     ui->textEdit->append ( "bytesAvailable: " + QString::number(storage.bytesAvailable()/1024/1024) + " MB");   
     ui->textEdit->append ( "bytesFree: " + QString::number(storage.bytesFree()/1024/1024) + " MB");
   //  ui->textEdit->append ( ".");
-    //TODO надо посчитать процент доступного места
+    // надо посчитать процент доступного места
 //    Сколько процентов составляет 24 от числа 248 ?
 //            Итог - 9.677 %
 //        Как вычислять:
@@ -82,12 +103,38 @@ void Dialog::updateTime()
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// //    Сколько процентов составляет доступное пространство от полного пространства ?
     //            Итог - 9.677 %
-    //        Как вычислять:
+    //        Как вычислять:;
     //        Получаем коэффициент - полное пространство / доступное пространство = 10.333
     //            Получаем проценты - 100% / ( полное пространство / доступное пространство) = 9.677 %
-    
+//    int peremennaya_polnoe_prostranstvo;
+//    int peremennaya_dostupnoe_prostranstvo;
+//    int protsent_dostupnogo_prostranstva;
+    peremennaya_polnoe_prostranstvo=storage.bytesTotal()/1024/1024;
+    peremennaya_dostupnoe_prostranstvo=storage.bytesAvailable()/1024/1024;
+    int x =peremennaya_polnoe_prostranstvo/peremennaya_dostupnoe_prostranstvo;
+    protsent_dostupnogo_prostranstva=100/(x);
+    ui->textEdit->append ( "Процент доступного пространства: " + QString::number(protsent_dostupnogo_prostranstva) + " %");
 /// 
-/// 
+/// проверка величины:
+    if (protsent_dostupnogo_prostranstva<=15)
+    {
+      //  ;; // TODO beep
+         ui->textEdit->append ( "Тревога - доступное пространство равно 15%!");
+         tmr->stop(); // остановим таймер
+//    a:
+//        QThread::sleep (2);
+        QApplication:: beep () ;
+    a:
+        //        QThread::sleep (2);
+        // звуковой сигнал:
+        QProcess().execute("/home/viktor/my_scripts_2/sound.sh");
+      //  Вам просто нужно установить утилиту SoX.
+        // https://forum.qt.io/topic/54162/solved-qapplication-beep-no-sound-on-linux/15
+            // QThread::sleep (2);
+        goto a;
+//        QThread::sleep (2);
+//        goto a;
+    }
 ///     
 }
 
